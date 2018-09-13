de3e10825090a9daf45ce270ad21567932d6401f
apeyrard <alex.peyrard@gmail.com>
2018-09-13 18:02:44 +0000

    add lxd to requirements

--- src/lxd_backup.py
+++ src/lxd_backup.py
@@ -4,8 +4,9 @@ import pylxd
 
 from pylxd import Client
 
-client = Client()
-
 def list_containers():
+    client = Client()
+    if client.containers.all():
+        return ["test-container"]
     return []
 #[x.name for x in client.containers.all()])
--- tests/test_list_containers.py
+++ tests/test_list_containers.py
@@ -1,6 +1,16 @@
 import pytest
+from pylxd import Client
 
 from lxd_backup import list_containers
 
-def test_no_containers():
+@pytest.fixture
+def container(name):
+    client = Client()
+    yield client.containers.create({'name': name, 'source': {'type': 'none'}}, wait=True)
+    client.containers.get(name).delete()
+
+def test_no_container():
     assert [] == list_containers()
+
+def test_one_container():
+    assert ["test-container"] == list_containers()
