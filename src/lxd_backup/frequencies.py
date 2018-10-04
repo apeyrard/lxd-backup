2cb660eabda69801b75e72aa1275592b7b52b261
Alex Peyrard <alex.peyrard@gmail.com>
2018-10-04 19:15:02 +0200

    simplify frequencies

--- src/lxd_backup/frequencies.py
+++ src/lxd_backup/frequencies.py
@@ -4,27 +4,39 @@ from .time import (weekday,
                    month)
 
 
-def should_backup(frequency, target=None):
-    if frequency == Frequency.DAILY:
-        return True
-    elif frequency == Frequency.WEEKLY:
-        if target is None:
-            target = 0
-        return weekday() == target
-    elif frequency == Frequency.MONTHLY:
-        if target is None:
-            target = 1
-        return day() == target
-    elif frequency == Frequency.BIANUALLY:
-        if target is None:
-            target = {'month': 1, 'day': 1}
-        return month() % 6 == target['month'] and day() == target['day']
-    else:
-        return False
-
-
 class Frequency(Enum):
     DAILY = 0
     WEEKLY = 1
     MONTHLY = 2
     BIANUALLY = 3
+
+
+def get_default_target(frequency):
+    default_target = {
+        Frequency.DAILY: None,
+        Frequency.WEEKLY: 0,
+        Frequency.MONTHLY: 1,
+        Frequency.BIANUALLY: {'month': 1, 'day': 1}
+        }
+    return default_target[frequency]
+
+
+def should_backup(frequency, target=None):
+    if target is None:
+        try:
+            target = get_default_target(frequency)
+        except KeyError:
+            return False
+
+    if frequency == Frequency.DAILY:
+        return True
+    elif frequency == Frequency.WEEKLY:
+        return weekday() == target
+    elif frequency == Frequency.MONTHLY:
+        return day() == target
+    elif frequency == Frequency.BIANUALLY:
+        return month() % 6 == target['month'] and day() == target['day']
+    else:
+        return False
+
+
